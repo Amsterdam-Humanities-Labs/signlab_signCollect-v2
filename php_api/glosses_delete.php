00d21b482ef4905a2e18767826e8ff5d4eb0ea1a
rem0g <gotterspeer@gmail.com>
2026-05-21 18:06:27 +0000

    glosses_delete: route DELETE through dataset registry

--- php_api/glosses_delete.php
+++ php_api/glosses_delete.php
@@ -8,11 +8,15 @@ $body = json_body();
 $id   = (int)($body['id'] ?? 0);
 if ($id <= 0) json_response(['error' => 'invalid_id'], 400);
 
-$pdo = db();
+require_once __DIR__ . '/datasets.php';
+$pdo   = db();
+$ds    = require_dataset($pdo, $session, $body);
+$table = $ds['table'];
+
 $logEntry = "Verborgen op " . date('j/n/Y @ H:i') . " door: " . ($session['username'] ?: $session['userId']);
 
 $stmt = $pdo->prepare(
-    "UPDATE form_data
+    "UPDATE `$table`
      SET glosZichtbaar = 1,
          logboek = CONCAT_WS('\n', NULLIF(CONVERT(logboek USING utf8mb4), ''), ?)
      WHERE id = ?"
